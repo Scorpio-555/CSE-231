b7c7b7726f14e85dbb664e200fe1ec487e30ea00
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-09-28 17:48:39 -0600

    Update pawn.h

--- c- Week 03/Lab/pawn.h
+++ c- Week 03/Lab/pawn.h
@@ -6,11 +6,16 @@ class Pawn : public Piece
 {
 public:
 	Pawn(Color color, Point point);
-	bool move(Point point);
+	
 	list<Point> getAttackSquares();
 	list<Point> getPossibleMoves();
+
+	static void resetEnPotentialEnPassant() { potentialEnPassant = Point(-1,-1); }
+
+	bool move(Point point);
 	void draw();
 private:
+	static Point potentialEnPassant;
 	int firstRank;
 	int enPassantRank;
 	int lastRank;
