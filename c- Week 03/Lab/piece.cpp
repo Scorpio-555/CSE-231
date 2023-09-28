c7f7302a4b8c1ec5e3ee298bc45e2447887e3906
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-09-28 12:15:02 -0600

    Create piece.cpp

---
+++ c- Week 03/Lab/piece.cpp
@@ -0,0 +1,25 @@
+#include "piece.h"
+
+bool Piece::move(Point point)
+{
+    /*IF newPosition IN getPosibleMoves()
+
+        // standard stuff shared by all chess pieces
+
+        target <-- Game.getPieceAt(newPosition)
+        emptyTarget <-- (target = NULL)
+
+        IF NOT emptyTarget
+            IF target.getName() != "King"
+                target.kill()
+            ELSE
+                RETURN FALSE
+
+        oldPosition <-- position
+        position <-- newPosition
+
+        return true
+    */
+
+    return false;
+}
