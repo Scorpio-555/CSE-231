99578f31d3dc7497a2c98f0fca0361102d837142
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-09-28 12:24:31 -0600

    Update main.cpp

--- c- Week 03/Lab/main.cpp
+++ c- Week 03/Lab/main.cpp
@@ -18,7 +18,33 @@ int main()
 {
     Pawn pawn(Color::BLACK, Point());
 
-    cout << (pawn.getColor() == Color::BLACK) << endl;
+    if (pawn.getColor() == Color::BLACK) {
+        cout << "Color is black: true" << endl;
+    }
+    else {
+        cout << "Color is black: false" << endl;
+    }
+    
+    if (pawn.getName() == Name::PAWN) {
+        cout << "Name is Pawn: true" << endl;
+    }
+    else {
+        cout << "Name is Pawn: false" << endl;
+    }
+
+    if (pawn.isAlive() == true) {
+        cout << "isAlive: true" << endl;
+    }
+    else {
+        cout << "isAlive: false" << endl;
+    }
+
+    if (pawn.getPosition().getX() == 0 && pawn.getPosition().getY() == 0) {
+        cout << "position is (0,0): true" << endl;
+    }
+    else {
+        cout << "position is (0,0): false" << endl;
+    }
 
     return 0;
 }
