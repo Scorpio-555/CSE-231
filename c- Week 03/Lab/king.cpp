0b957d3f7b29ed80afaa8907e6c116f7c42f813d
Jacob Johnson <jakeymacaroni@gmail.com>
2023-09-28 17:37:13 -0600

    added king.h

---
+++ c- Week 03/Lab/king.cpp
@@ -0,0 +1,8 @@
+//
+//  king.cpp
+//  
+//
+//  Created by Jacob on 9/28/23.
+//
+
+#include "king.hpp"
---
+++ c- Week 03/Lab/king.hpp
@@ -0,0 +1,13 @@
+//
+//  king.hpp
+//  
+//
+//  Created by Jacob on 9/28/23.
+//
+
+#ifndef king_hpp
+#define king_hpp
+
+#include <stdio.h>
+
+#endif /* king_hpp */
