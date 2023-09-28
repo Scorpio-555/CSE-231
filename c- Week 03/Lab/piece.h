544d753018ae3dbf43c2d81dff2b5077ba324a32
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-09-28 12:11:20 -0600

    Create piece.h

---
+++ c- Week 03/Lab/piece.h
@@ -0,0 +1,35 @@
+#include <string>
+#include <list>
+#include "enum.h"
+#include "point.h"
+
+#pragma once
+
+using namespace std;
+
+class Piece
+{
+public:
+
+	// getters
+	Name getName() { return name; }
+	Point getPosition() { return position; }
+	Color getColor() { return color; }
+	bool isAlive() { return alive; }
+
+	//setters
+	void kill() { alive = false; }
+
+	// virtual functions
+	virtual bool move(Point point);
+
+	// pure virtual functions
+	virtual list<Point> getAttackSquares() = 0;
+	virtual list<Point> getPossibleMoves() = 0;
+	virtual void draw() = 0;
+protected:
+	Name name;
+	Color color;
+	Point position;
+	bool alive;
+};
