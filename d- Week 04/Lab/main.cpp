015689b89b2a08ffdf5f63a7d458a6140dc61652
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-09-30 14:20:42 -0600

    Create main.cpp

---
+++ d- Week 04/Lab/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>  // for CIN and COUT
+#include <string>
+#include <cmath>
+#include <numbers>
+#include<list>
+#include <cmath>
+#include <map>
+#include <iterator>
+#include <cassert>
+
+#include "testKing.h"
+#include "testGame.h"
+
+using namespace std;
+
+int main()
+{
+    // Game tets
+    TestGame tg;
+    tg.run();
+    cout << "Game tests PASSED" << endl;
+
+    // King tests
+    TestKing tk;
+    tk.run();
+    cout << "King tests PASSED" << endl;
+
+    cout << "ALL TESTS PASSED" << endl;
+
+    return 0;
+}
