c662b009ff95698a9e9ba169bc7da36be9da48c2
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-10-03 16:27:59 -0600

    Update testKing.h
    fixed typo in comments

--- d- Week 04/Lab/testKing.h
+++ d- Week 04/Lab/testKing.h
@@ -10,14 +10,14 @@ private:
 	void testTwoSpaceH();		// king should tell us he can't move two spaces
 	void testTwoSpaceV();
 	void testTwoSpaceD();
-	void testAttackH();			// horizontal, attack left and right
+	void testAttackH();		// horizontal, attack left and right
 	void testAttackV();
 	void testAttackD();
 	void testAttackComrade();	// king should tell us he can't attack comrade
 	void testMoveIntoCheck();	// king should tell us he can't move into check
 	void testMoveOutOfCheck();
 	void testStillInCheck();	// king should tell us he can't move to a place where he remains in check
-	void testCastle();        // when you can castle
+	void testCastle();        	// when you can castle
 	void testQueensCastle();
 	void testKingsCastle();   
 	void testFailedCastle();	// and when you can't
