8aecd6ec9e0a0f6fbbf77f7720a3de60c122ba35
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-10-03 16:32:06 -0600

    Update king.cpp
    some of getPossibleMoves has been implemented

--- d- Week 04/Lab/king.cpp
+++ d- Week 04/Lab/king.cpp
@@ -1,4 +1,5 @@
 #include "king.h"
+#include "game.h"
 
 King::King(Color color, Point point) {
     name = Name::KING;
@@ -8,12 +9,98 @@ King::King(Color color, Point point) {
     this->position = point;
 }
 
-list<Point> King::getAttackSquares() {
-    return list<Point>();
+set<int> King::getAttackSquares() {
+    return set<int>();
 }
 
 list<Point> King::getPossibleMoves() {
-    return list<Point>();
+    set<int> enemyAttackSquares = Game::getEnemyAttackSquares(color);
+    list<Point> possible = list<Point>();
+
+    //castling
+    if (hasMoved == false && Game::inCheck(color) == false) {
+        /*// look for left rook
+        leftCorner <-- position
+        leftCorner.setX(0)
+        leftCornerPiece <-- Game.getPieceAt(leftCorner)
+
+
+        IF leftCornerPiece NOT NULL AND leftCornerPiece.getName() = "Rook"
+            IF leftCornerPiece.getHasMoved() = FALSE
+                leftOne <-- position
+                leftOne.addX(-1)
+                leftOneEmpty <-- (Game.getPieceAt(leftOne) = NULL)
+
+
+                leftTwo <-- position
+                leftTwo.addX(-2)
+                leftTwoEmpty <-- (Game.getPieceAt(leftTwo) = NULL)
+
+
+                leftOneSafe <-- leftOne NOT IN enemyAttackSquares
+                leftTwoSafe <-- leftTwo NOT IN enemyAttackSquares
+
+
+                bothEmpty <-- leftOneEmpty AND leftTwoEmpty
+                bothSafe <-- leftOneSafe AND leftTwoSafe
+
+
+                IF bothEmpty AND bothSafe
+                    possible.insert(leftTwo) // queen's castle
+
+
+        // look for left rook
+        rightCorner <-- position
+        rightCorner.setX(7)
+        rightCornerPiece <-- Game.getPieceAt(rightCorner)
+
+
+        IF rightCornerPiece NOT NULL AND rightCornerPiece.getName() = "Rook"
+            IF rightCornerPiece.getHasMoved() = FALSE
+                rightOne <-- position
+                rightOne.addX(1)
+                rightOneEmpty <-- (Game.getPieceAt(rightOne) = NULL)
+
+
+                rightTwo <-- position
+                rightTwo.addX(2)
+                rightTwoEmpty <-- (Game.getPieceAt(rightTwo) = NULL)
+
+
+
+
+                rightOneSafe <-- rightOne NOT IN enemyAttackSquares
+                rightTwoSafe <-- rightTwo NOT IN enemyAttackSquares
+
+
+                bothEmpty <-- rightOneEmpty AND rightTwoEmpty 
+                bothSafe <-- rightOneSafe AND rightTwoSafe 
+
+
+                IF bothEmpty AND bothSafe
+                    possible.insert(rightTwo) // king's castle*/
+    }
+
+    list<Point> moves =
+    {
+        Point(-1, 1),  Point(0, 1),  Point(1, 1),
+        Point(-1, 0),                Point(1, 0),
+        Point(-1, -1), Point(0, -1), Point(1, -1)
+    };
+
+    for (Point move : moves) {
+        Point point = position + move;
+        if (point.inBounds()) {
+            Piece* piece = Game::getPieceAt(point);
+            bool isSafe = (enemyAttackSquares.find(point.getInt()) == enemyAttackSquares.end());
+
+            if (isSafe && (piece == nullptr || piece->getColor() != color)) {
+                possible.push_back(point);
+            }
+        }
+    }
+
+    return possible;
 }
 
 void King::draw()
