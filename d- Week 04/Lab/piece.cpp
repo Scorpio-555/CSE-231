72490685ade5e7f26058c0c98af819a1f3aa6334
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-10-03 16:38:35 -0600

    Update piece.cpp
    jeopardizeKing implemented

--- d- Week 04/Lab/piece.cpp
+++ d- Week 04/Lab/piece.cpp
@@ -1,9 +1,33 @@
 #include "piece.h"
 #include "game.h"
 
+bool Piece::jeopardizeKing(Point newPosition, bool iAmGuardingKing, bool inCheck)
+{
+    // we either aren't in check, or we want to move to a square that gets us out of check
+    if (inCheck == false || Game::getCheckingPath().find(newPosition.getInt()) != Game::getCheckingPath().end()) {
+        // our movement has the potential to put the king in check
+        if (iAmGuardingKing) {
+            Point kingPoint = Game::getKing(color)->getPosition();
+            Point directionToKing = Game::getDirection(position, kingPoint);
+            Point directionAwayFromKing = Game::getDirection(kingPoint, position);
+            Point desiredDirection = Game::getDirection(position, newPosition);
+            // we can only move directly toward the king, or directly away, otherwise we will put the king in check
+            return (desiredDirection == directionToKing || desiredDirection == directionAwayFromKing);
+        }
+        // we have no potential to put the king in check
+        else {
+            return false;
+        }
+    }
+    // we are in check and the square we want to move to will not get us out of check
+    else {
+        return true;
+    }
+}
+
 bool Piece::move(Point newPosition)
 {
-    bool moveFound = false;
+    /*bool moveFound = false;
 
     for (Point point : getPossibleMoves()) {
         if (newPosition.getInt() == point.getInt()) {
@@ -29,5 +53,8 @@ bool Piece::move(Point newPosition)
         return true;
     }
 
-    return false;
+    return false;*/
+
+    position = newPosition;
+    return true;
 }
