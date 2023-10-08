053f88571abf97e3c57ba1a886b863e50821a164
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-10-07 23:33:49 -0600

    Create knight.cpp

---
+++ e- Week 05/Lab/knight.cpp
@@ -0,0 +1,56 @@
+#include "knight.h"
+#include "game.h"
+
+Knight::Knight(Color color, Point position)
+{
+	name = Name::KNIGHT;
+	alive = true;
+	this->color = color;
+	this->position = position;
+}
+
+set<int> Knight::getAttackSquares()
+{
+	set<int> attackSquares = set<int>();
+
+	list<Point> moves =
+	{
+				 Point(-1, 2),		  Point(1, 2),
+		Point(-2, 1),				   		   Point(2, 1),
+
+		Point(-2, -1),				   		   Point(2, -1),
+				 Point(-1, -2),		  Point(1, -2)
+	};
+	
+	for (Point move : moves) {
+		Point point = position + move;
+		if (point.inBounds()) {
+			attackSquares.insert(point.getInt());
+		}
+	}
+
+	return attackSquares;
+}
+
+list<Point> Knight::getPossibleMoves()
+{
+	list<Point> possible = list<Point>();
+	set<int> attackSquares = getAttackSquares();
+	bool guardingKing = Game::amIGuardingKing(getPosition());
+	bool inCheck = Game::inCheck(color);
+
+	for (int square : attackSquares) {
+		if (jeopardizeKing(Point(square), guardingKing, inCheck) == false) {
+			Piece* piece = Game::getPieceAt(Point(square));
+			if (piece == nullptr || piece->getColor() != color) {
+				possible.push_back(Point(square));
+			}
+		}
+	}
+
+	return possible;
+}
+
+void Knight::draw()
+{
+}
