1388b37a355dcb6e08ccfdbc4d4d9fb45a241682
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-10-07 23:24:20 -0600

    Create king.h

---
+++ e- Week 05/Lab/king.h
@@ -0,0 +1,19 @@
+#include "piece.h"
+
+#pragma once
+
+class King : public Piece
+{
+public:
+    King(Color color, Point point);
+
+    bool getHasMoved() { return hasMoved; }
+    set<int> getAttackSquares();
+    list<Point> getPossibleMoves();
+
+    void draw();
+    bool move(Point newPosition);
+
+private:
+    bool hasMoved;
+};
