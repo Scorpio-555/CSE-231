7f73c32204a15f38a4962b7f872134549e88cff4
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-10-07 23:23:45 -0600

    Create game.h

---
+++ e- Week 05/Lab/game.h
@@ -0,0 +1,44 @@
+#include <list>
+#include <set>
+#include "piece.h"
+
+#pragma once
+
+class TestGame;
+
+class Game {
+	friend class TestGame;
+public:
+	static Piece* getPieceAt(Point point);
+	static Piece* getKing(Color color);
+	static set<int> getEnemyAttackSquares(Color yourColor);
+	static set<int> getCheckingPath() { return checkingPath; }
+
+	static void addPiece(Piece * piece);
+	static void toggleTurnHolder();
+
+	static bool amIGuardingKing(Point point);
+	static Point getDirection(Point startingPoint, Point secondPoint);
+	static bool movePiece(Point from, Point to);
+	static void draw(Point hoverPos, Point selectPos);
+
+	static bool isEndGame();
+	static bool inCheck(Color yourColor);
+	static bool isCheckmate() { return checkmate; }
+	static bool isStalemate() { return stalemate; }
+
+	static void removeZombies();
+	static void reset();
+	static void newGame();
+
+private:
+	static void determineCheck();
+
+	static list<Piece*> pieces;
+	static Color currentTurnHolder;
+	static Piece* kingInCheck;
+	static list<Piece*> checkingPieces;
+	static set<int> checkingPath;
+	static bool checkmate;
+	static bool stalemate;
+};
