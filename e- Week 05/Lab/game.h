c11f11a88410963cb8b37a9adf16cfcba3b11626
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-10-10 16:43:16 -0600

    Update game.h
    added getTurnHolder()

--- e- Week 05/Lab/game.h
+++ e- Week 05/Lab/game.h
@@ -4,23 +4,21 @@
 
 #pragma once
 
-class TestGame;
-
 class Game {
-	friend class TestGame;
 public:
+	static Color getTurnHolder() { return currentTurnHolder; }
 	static Piece* getPieceAt(Point point);
 	static Piece* getKing(Color color);
 	static set<int> getEnemyAttackSquares(Color yourColor);
 	static set<int> getCheckingPath() { return checkingPath; }
 
-	static void addPiece(Piece * piece);
+	static void addPiece(Piece* piece);
 	static void toggleTurnHolder();
 
 	static bool amIGuardingKing(Point point);
 	static Point getDirection(Point startingPoint, Point secondPoint);
 	static bool movePiece(Point from, Point to);
-	static void draw(Point hoverPos, Point selectPos);
+	static void draw(int hoverPos, int selectPos);
 
 	static bool isEndGame();
 	static bool inCheck(Color yourColor);
