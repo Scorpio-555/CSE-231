f52ea572c35672f499d1944bf563091aba1556e2
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-10-28 10:40:51 -0600

    Update trig.h
    just looks more tidy

--- g- Week 07/Lab 7/231/trig.h
+++ g- Week 07/Lab 7/231/trig.h
@@ -11,20 +11,8 @@ public:
 	static double computeSineComponent(double radians, double total) { return total * sin(radians); }
 	static double computeCosineComponent(double radians, double total) { return total * cos(radians); }
 	static double computeTotalComponent(double horizontalComponent, double verticalComponent) { return sqrt(pow(horizontalComponent, 2) + pow(verticalComponent, 2)); }
-	/*static double distanceBetweenPoints(Position startPt, Position endPt) { return sqrt(pow((endPt.getMetersX() - startPt.getMetersX()), 2) + pow((endPt.getMetersY() - startPt.getMetersY()), 2)); }*/
-	static double distanceBetweenPoints(Position startPt, Position endPt) { 
-		double distance = 0.0;
-		double x1 = startPt.getMetersX();
-		double y1 = startPt.getMetersY();
-		double x2 = endPt.getMetersX();
-		double y2 = endPt.getMetersY();
-
-		distance = sqrt(pow((endPt.getMetersX() - startPt.getMetersX()), 2) + pow((endPt.getMetersY() - startPt.getMetersY()), 2));
-
-		return distance; 
-	}
+	static double distanceBetweenPoints(Position startPt, Position endPt) { return sqrt(pow((endPt.getMetersX() - startPt.getMetersX()), 2) + pow((endPt.getMetersY() - startPt.getMetersY()), 2)); }
 	static double areaOfCircle(double radius) { return PI * radius * radius; }
 	static double getAngle(double dx, double dy) { return atan2(dx, dy); }
 
 };
-
