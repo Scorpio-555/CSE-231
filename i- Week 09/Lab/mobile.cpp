210dabd45ceb8f0865d0cbaa6df9549f0382e719
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-11-08 22:41:09 -0700

    Create mobile.cpp

---
+++ i- Week 09/Lab/mobile.cpp
@@ -0,0 +1,44 @@
+#include "mobile.h"
+
+Position Mobile::getPosition()
+{
+	return Position();
+}
+
+double Mobile::getRadius()
+{
+	return 0.0;
+}
+
+double Mobile::getHeight()
+{
+	return 0.0;
+}
+
+double Mobile::get_gH()
+{
+	return 0.0;
+}
+
+double Mobile::getGravityDdx()
+{
+	return 0.0;
+}
+
+double Mobile::getGravityDdy()
+{
+	return 0.0;
+}
+
+bool Mobile::isAlive()
+{
+	return false;
+}
+
+void Mobile::kill()
+{
+}
+
+void Mobile::move()
+{
+}
