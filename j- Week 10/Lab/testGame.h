fe5df8a62cf475184c1785e1ce918a9d92af0403
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-11-16 12:58:39 -0700

    Create testGame.h
    original version from week 09

---
+++ j- Week 10/Lab/testGame.h
@@ -0,0 +1,15 @@
+#pragma once
+class TestGame {
+public:
+	void run();
+private:
+	void testGetTimeInterval();
+	void testGetEarthRadius();
+	void testGetEarthPosition();
+	void testGetEarthRotationSpeed();
+	void testAddMobile();
+	void testAdvance();
+	void testCheckCollisions();
+	void testKillShip();
+	void testRemoveZombies();
+};
