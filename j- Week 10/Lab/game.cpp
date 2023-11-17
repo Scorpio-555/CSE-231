41070ef2cbe3289f110c68aa0eac2acfc3e7313e
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-11-16 18:24:16 -0700

    Update game.cpp
    game tests pass

--- j- Week 10/Lab/game.cpp
+++ j- Week 10/Lab/game.cpp
@@ -1,9 +1,10 @@
 #include "game.h"
+#include "trig.h"
 
 double Game::timeInterval = 0.0;
 Earth Game::earth = Earth();
 Ship* Game::ship = nullptr;
-list<Mobile> Game::mobileList = list<Mobile>();
+list<Mobile*> Game::mobileList = list<Mobile*>();
 
 double Game::getTimeInterval()
 {
@@ -27,20 +28,51 @@ double Game::getEarthRotationSpeed()
 
 void Game::advance()
 {
+	for (Mobile* m : mobileList) {
+		m->move();
+	}
 }
 
 void Game::checkCollisions()
 {
+	for (auto it = mobileList.begin(); it != mobileList.end(); ++it) {
+		Mobile* mobile1 = *it;
+		if (mobile1->getHeight() <= mobile1->getRadius()) {
+			mobile1->kill();
+		}
+
+		auto it2 = it;
+		++it2;
+
+		for (it2; it2 != mobileList.end(); ++it2) {
+			Mobile* mobile2 = *it2;
+			double distance = Trig::distanceBetweenPoints(mobile1->getPosition(), mobile2->getPosition());
+			double threshold = mobile1->getRadius() + mobile2->getRadius();
+			if (distance <= threshold) {
+				mobile1->kill();
+				mobile2->kill();
+			}
+		}
+	}
 }
 
 void Game::removeZombies()
 {
+	for (auto it = mobileList.begin(); it != mobileList.end();)
+		if ((*it)->isAlive() == false)
+		{
+			it = mobileList.erase(it);
+		}
+		else
+			++it;
 }
 
-void Game::addMobile(Mobile mobile)
+void Game::addMobile(Mobile* mobile)
 {
+	mobileList.push_back(mobile);
 }
 
 void Game::killShip()
 {
+	ship = nullptr;
 }
