723316a699589946288279c037038b926f433b42
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-11-18 10:16:02 -0700

    Create testMobile.h

---
+++ j- Week 10/Lab/testMobile.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdio.h>
+class TestMobile {
+public:
+    void run();
+private:
+    void testSputnik();
+    void testGPS();
+    void testHubble();
+    void testDragon();
+    void testStarlink();
+    void testGPSCenter();
+    void testShip();
+};
